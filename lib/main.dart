b3e4828ba533c45307e0de59895247df4da8d160
Gerick M. Velasquez <teenbritish11@gmail.com>
2025-03-24 17:12:31 +0800

    gerick pull and push

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 // import 'package:supabase_flutter/supabase_flutter.dart';
 
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
