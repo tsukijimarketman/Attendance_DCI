496b06283adba18d3818a62ae9eca93c8136516b
Aquafritz <aquinofrederick2003@gmail.com>
2025-05-16 14:17:16 +0800

    Pulling

--- lib/Auth/login.dart
+++ lib/Auth/login.dart
@@ -60,17 +60,6 @@ class _LoginState extends State<Login> with TickerProviderStateMixin {
   late AnimationController controllerSignin;
   late Animation<double> _textRevealcontrollerSignin;
   late Animation<double> _textOpacitycontrollerSignin;
-  final String url = 'https://attendance-dci.web.app/privacy-policy/';
-
-  Future<void> _launchURL(BuildContext context) async {
-    if (await canLaunch(url)) {
-      await launch(url, webOnlyWindowName: '_blank'); // Opens in new tab on web
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Could not open Privacy Policy')),
-      );
-    }
-  }
 
   // this is all for the animation of the text field
   @override
@@ -393,37 +382,6 @@ class _LoginState extends State<Login> with TickerProviderStateMixin {
               ),
             ),
             SizedBox(
-              height: MediaQuery.of(context).size.width / 70,
-            ),
-            RichText(
-      text: TextSpan(
-        text: 'By signing in, you agree to our ',
-        style: TextStyle(color: Colors.white, fontSize: 14),
-        children: [
-          TextSpan(
-            text: 'Privacy Policy',
-            style: TextStyle(
-              color: Colors.blue,
-              fontSize: 18,
-              decoration: TextDecoration.underline,
-              fontWeight: FontWeight.bold,
-            ),
-            recognizer: TapGestureRecognizer()
-              ..onTap = () async {
-                if (await canLaunch(url)) {
-                  await launch(url, webOnlyWindowName: '_blank'); // open new tab on web
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Could not open Privacy Policy')),
-                  );
-                }
-              },
-          ),
-          TextSpan(text: '.'),
-        ],
-      )
-    ).showCursorOnHover.moveUpOnHover,
-     SizedBox(
               height: MediaQuery.of(context).size.width / 70,
             ),
             Row(
